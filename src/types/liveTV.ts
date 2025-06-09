65e2c81d093f214f4de4b2cb91ff0016e061b2d1
fgrtgtrgtrgrt <127044684+fgrtgtrgtrgrt@users.noreply.github.com>
2025-06-08 22:27:44 -0400

    Update liveTV.ts

--- src/types/liveTV.ts
+++ src/types/liveTV.ts
@@ -1,13 +1,15 @@
-
 export interface LiveTVChannel {
   id: string;
   name: string;
   logo: string;
   category: string;
   country: string;
-  language: string;
+  language?: string;
   url: string;
   isWorking: boolean;
+  currentProgram?: string;
+  programStart?: string;
+  programEnd?: string;
 }
 
 export interface LiveTVCategory {
